25e27c99dc32db35bee97ebc816e05c8990d0e8c
Hudson Mendes <hudsonmendes@gmail.com>
2022-05-31 20:07:19 +0100

    joint: moving now

--- bot101.py
+++ bot101.py
@@ -3,7 +3,6 @@ import utils
 plane_shape, floor = utils.setup_env(p)
 
 bot101 = p.loadURDF('bot101.urdf')
+p.setJointMotorControl2(bot101, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=0.25)
 
-print(p.isNumpyEnabled())
-p.setGravity(0, 0, -10)
 utils.wait_ctrl_c(p)
--- utils.py
+++ utils.py
@@ -8,11 +8,13 @@ def  setup_env(p):
     p.connect(p.GUI)
     p.setPhysicsEngineParameter(enableFileCaching=0)
     p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+    p.setGravity(0, 0, -10)
     plane_shape = p.createCollisionShape(p.GEOM_PLANE)
     floor = p.createMultiBody(plane_shape, plane_shape)
     return plane_shape, floor
 
 def wait_ctrl_c(p):
+    p.setRealTimeSimulation(1)
     print("Press CTRL+C to interrupt...")
     import time
     while True:
