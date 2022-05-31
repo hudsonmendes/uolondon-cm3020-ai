a6710b36d9c6c5289581cdcfe710e2b28ae8f18d
Hudson Mendes <hudsonmendes@gmail.com>
2022-05-31 20:19:50 +0100

    bots are moving!

--- bot101.py
+++ bot101.py
@@ -3,6 +3,7 @@ import utils
 plane_shape, floor = utils.setup_env(p)
 
 bot101 = p.loadURDF('bot101.urdf')
-p.setJointMotorControl2(bot101, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=0.25)
+p.setJointMotorControl2(bot101, 0, controlMode=p.VELOCITY_CONTROL, targetVelocity=0.25, targetPosition=0.5)
+p.setJointMotorControl2(bot101, 1, controlMode=p.POSITION_CONTROL, targetVelocity=0.5, targetPosition=0.25)
 
 utils.wait_ctrl_c(p)
