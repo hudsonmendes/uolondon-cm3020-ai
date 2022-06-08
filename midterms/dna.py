706eb3f8eb8529d2a6a1e63d8d5276d2c9de9758
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-08 22:26:27 +0100

    dna: ensuring alignment

--- midterms/dna.py
+++ midterms/dna.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentError
 from typing import List
 
 
@@ -7,10 +6,24 @@ class Dna:
     gene_len: int
 
     def __init__(self, code: List[float], gene_len: int) -> None:
-        DnaCodeVoid.raise_if_applicable_from(dna_code=code)
-        DnaCodeMisalignedException.raise_if_applicable_from(dna_code_len=len(code), gene_len=gene_len)
         self.code = code
+        self.code_len = len(code)
         self.gene_len = gene_len
+        if gene_len:
+            self.gene_count = self.code_len // gene_len
+            if self.code_len % gene_len == 0:
+                self.gene_count -= 1
+        DnaCodeVoid.raise_if_applicable_from(dna_code=self.code)
+        DnaGenVoid.raise_if_applicable_from(gene_len=self.gene_len)
+        DnaCodeMisalignedException.raise_if_applicable_from(dna_code_len=self.code_len, gene_count=self.gene_count, gene_len=self.gene_len)
+
+    def express(self) -> List[List[float]]:
+        out: List[List[float]] = []
+        for i in range(0, self.gene_count):
+            gene_begin = i*self.gene_len
+            gene_end = (i+1)*self.gene_len
+            out.append(self.code[gene_begin:gene_end])
+        return out
 
 
 class DnaCodeVoid(Exception):
@@ -26,13 +39,23 @@ class DnaCodeVoid(Exception):
         """)
 
 
+class DnaGenVoid(Exception):
+    @staticmethod
+    def raise_if_applicable_from(gene_len: int):
+        if gene_len <= 0:
+            raise DnaGenVoid(gene_len=gene_len)
+
+    def __init__(self, gene_len: int):
+        super(DnaGenVoid, self).__init__(f"""
+        The `gene_len={gene_len}` cannot have length <= 0.
+        """)
+
+
 class DnaCodeMisalignedException(Exception):
     @staticmethod
-    def raise_if_applicable_from(dna_code_len: int, gene_len: int):
-        if gene_len:
-            gene_count = dna_code_len // gene_len
-            if dna_code_len != (gene_count * gene_len) + gene_count:
-                raise DnaCodeMisalignedException(dna_code_len=dna_code_len, gene_len=gene_len)
+    def raise_if_applicable_from(dna_code_len: int, gene_count: int, gene_len: int):
+        if dna_code_len != (gene_count * gene_len) + gene_count:
+            raise DnaCodeMisalignedException(dna_code_len=dna_code_len, gene_len=gene_len)
 
     def __init__(self, dna_code_len: int, gene_len: int):
         super(DnaCodeMisalignedException, self).__init__(f"""
--- midterms/test_dna.py
+++ midterms/test_dna.py
@@ -10,7 +10,8 @@ class DnaTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.gene_len = genome.Genome.get_gene_len()
-        self.dna_code = [random.random() for _ in range((self.gene_len * 3) + 3)]
+        self.gene_count = 3
+        self.dna_code = [random.random() for _ in range((self.gene_len * self.gene_count) + self.gene_count)]
         self.subject = dna.Dna(code=self.dna_code, gene_len=self.gene_len)
 
     def test_exists(self):
@@ -24,9 +25,15 @@ class DnaTest(unittest.TestCase):
 
     def test_ensures_code_not_empty_for_any_length(self):
         dna_code = []
-        for gene_len in [0, 1, random.randint(2, 100)]:
+        for gene_len in [1, random.randint(2, 100)]:
             with self.assertRaises(dna.DnaCodeVoid):
                 dna.Dna(code=dna_code, gene_len=gene_len)
+
+    def test_ensures_gene_len_not_void(self):
+        dna.Dna(code=[random.random()] * 2, gene_len=1)
+        dna.Dna(code=self.dna_code, gene_len=self.gene_len)
+        with self.assertRaises(dna.DnaGenVoid):
+            dna.Dna(code=self.dna_code, gene_len=0)
 
     def test_ensures_code_gene_len_aligned(self):
         for gene_count in range(1, 10):
@@ -36,3 +43,6 @@ class DnaTest(unittest.TestCase):
                 dna.Dna(code=dna_code[:-1], gene_len=self.gene_len)
             with self.assertRaises(dna.DnaCodeMisalignedException):
                 dna.Dna(code=dna_code[-1:], gene_len=self.gene_len)
+
+    def test_get_genes_expresses_all_genes_for_control_genes_all_above_treshold(self):
+        actual = self.subject.express()
