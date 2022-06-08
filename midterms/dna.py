106f0be2ebfc69b0d95fec014afe996e4347a22a
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-08 23:17:04 +0100

    dna: comments

--- midterms/dna.py
+++ midterms/dna.py
@@ -4,12 +4,32 @@ from typing import List, Tuple
 
 @dataclass
 class Dna:
+    """
+    Holds the genetic code, and the information about
+    Feature Genes (that may be expressed as phenotypes) and
+    Control Genes (that will allow/disallow Feature genes to be expressed).
+    """
     code: List[float]
     genes_features: List[List[float]]
     genes_control: List[bool]
 
+    def express(self) -> List[List[float]]:
+        """
+        Expresses feature genes allowed by control genes.
+        Suppressed feature genes may still be carried forward by elite Creatures.
+        """
+        expressed: List[List[float]] = []
+        for feature, control in zip(self.genes_features, self.genes_control):
+            if control:
+                expressed.append(feature)
+        return expressed
+
     @staticmethod
     def read(data: str, gene_len: int = 1) -> "Dna":
+        """
+        Reads the DNA code and splits it into feature
+        genes and control genes, producing an instance of DNA
+        """
         assert data and len(data.split(',')) > gene_len
         dna_code = Dna._read_dna_code_from(data)
         features, controls = Dna._read_genes_from(dna_code, gene_len)
