aa8488b983461370ef4ad05e215915702a57fbb7
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-08 23:03:22 +0100

    dna: data reading ok

--- midterms/dna.py
+++ midterms/dna.py
@@ -1,66 +1,33 @@
-from typing import List
+from dataclasses import dataclass
+from typing import List, Tuple
 
 
+@dataclass
 class Dna:
     code: List[float]
-    gene_len: int
+    genes: List[List[float]]
+    controls: List[bool]
 
-    def __init__(self, code: List[float], gene_len: int) -> None:
-        self.code = code
-        self.code_len = len(code)
-        self.gene_len = gene_len
-        if gene_len:
-            self.gene_count = self.code_len // gene_len
-            if self.code_len % gene_len == 0:
-                self.gene_count -= 1
-        DnaCodeVoid.raise_if_applicable_from(dna_code=self.code)
-        DnaGenVoid.raise_if_applicable_from(gene_len=self.gene_len)
-        DnaCodeMisalignedException.raise_if_applicable_from(dna_code_len=self.code_len, gene_count=self.gene_count, gene_len=self.gene_len)
-
-    def express(self) -> List[List[float]]:
-        out: List[List[float]] = []
-        for i in range(0, self.gene_count):
-            gene_begin = i*self.gene_len
-            gene_end = (i+1)*self.gene_len
-            out.append(self.code[gene_begin:gene_end])
-        return out
-
-
-class DnaCodeVoid(Exception):
     @staticmethod
-    def raise_if_applicable_from(dna_code: List[float]):
-        if not dna_code:
-            raise DnaCodeVoid(dna_code_len=len(dna_code))
+    def read(data: str, gene_len: int = 1) -> "Dna":
+        dna_code = Dna._read_dna_code_from(data)
+        genes, controls = Dna._read_genes_from(dna_code, gene_len)
+        return Dna(code=dna_code, genes=genes, controls=controls)
 
-    def __init__(self, dna_code_len: int):
-        super(DnaCodeVoid, self).__init__(f"""
-        The DNA `code` must not be void.
-        `len(code)={dna_code_len}` is not a valid length.
-        """)
-
-
-class DnaGenVoid(Exception):
     @staticmethod
-    def raise_if_applicable_from(gene_len: int):
-        if gene_len <= 0:
-            raise DnaGenVoid(gene_len=gene_len)
+    def _read_dna_code_from(data: str) -> List[float]:
+        assert data
+        return [float(gene) for gene in data.split(',')]
 
-    def __init__(self, gene_len: int):
-        super(DnaGenVoid, self).__init__(f"""
-        The `gene_len={gene_len}` cannot have length <= 0.
-        """)
-
-
-class DnaCodeMisalignedException(Exception):
     @staticmethod
-    def raise_if_applicable_from(dna_code_len: int, gene_count: int, gene_len: int):
-        if dna_code_len != (gene_count * gene_len) + gene_count:
-            raise DnaCodeMisalignedException(dna_code_len=dna_code_len, gene_len=gene_len)
-
-    def __init__(self, dna_code_len: int, gene_len: int):
-        super(DnaCodeMisalignedException, self).__init__(f"""
-        The DNA `code` is misaligned with the `gen_len` and is Lethal.
-        `len(code)={dna_code_len}` must be formed of `(n * gene_len={gene_len}) + n` bases,
-        where the `(n * gene_len)` component are the genes themselves
-        and the `+ n` component is the expression control mechanism.
-        """)
+    def _read_genes_from(dna_code: List[float], gene_len: int) -> Tuple[List[List[float]], List[bool]]:
+        assert len(dna_code) > gene_len
+        genes: List[List[float]] = []
+        controls: List[bool] = []
+        i: int = 0
+        end: int = len(dna_code)
+        while i < end:
+            genes.append(dna_code[i:i+gene_len])
+            i += gene_len
+            end -= 1
+        return genes, controls
--- midterms/test_dna.py
+++ midterms/test_dna.py
@@ -1,48 +1,33 @@
-from argparse import ArgumentError
 import unittest
-
 import random
+
 import dna
-import genome
 
 
 class DnaTest(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.gene_len = genome.Genome.get_gene_len()
-        self.gene_count = 3
-        self.dna_code = [random.random() for _ in range((self.gene_len * self.gene_count) + self.gene_count)]
-        self.subject = dna.Dna(code=self.dna_code, gene_len=self.gene_len)
-
-    def test_exists(self):
+    def test_class_exists(self):
         self.assertIsNotNone(dna.Dna)
 
-    def test_has_gene_len(self):
-        self.assertEquals(self.gene_len, self.subject.gene_len)
+    def test_read_code(self):
+        given = [random.random() for _ in range(random.randint(1, 100))]
+        actual = dna.Dna.read(",".join([str(gene) for gene in given]))
+        self.assertListEqual(given, actual.code)
 
-    def test_has_code(self):
-        self.assertEqual(self.dna_code, self.subject.code)
+    def test_read_genes_for_1_gene_len_1(self):
+        self.assertEqual([[0.5]], dna.Dna.read("0.5, 1", gene_len=1).genes)
 
-    def test_ensures_code_not_empty_for_any_length(self):
-        dna_code = []
-        for gene_len in [1, random.randint(2, 100)]:
-            with self.assertRaises(dna.DnaCodeVoid):
-                dna.Dna(code=dna_code, gene_len=gene_len)
+    def test_read_genes_for_2_gene_len_1(self):
+        self.assertEqual([[0.1], [0.2]], dna.Dna.read("0.1, 0.2, 1, 1", gene_len=1).genes)
 
-    def test_ensures_gene_len_not_void(self):
-        dna.Dna(code=[random.random()] * 2, gene_len=1)
-        dna.Dna(code=self.dna_code, gene_len=self.gene_len)
-        with self.assertRaises(dna.DnaGenVoid):
-            dna.Dna(code=self.dna_code, gene_len=0)
+    def test_read_genes_for_3_gene_len_1(self):
+        self.assertEqual([[0.1], [0.2], [0.3]], dna.Dna.read("0.1, 0.2, 0.3, 1, 1, 1", gene_len=1).genes)
 
-    def test_ensures_code_gene_len_aligned(self):
-        for gene_count in range(1, 10):
-            dna_code = [random.random() for _ in range((self.gene_len * gene_count) + gene_count)]
-            self.assertIsNotNone(dna.Dna(code=dna_code, gene_len=self.gene_len))
-            with self.assertRaises(dna.DnaCodeMisalignedException):
-                dna.Dna(code=dna_code[:-1], gene_len=self.gene_len)
-            with self.assertRaises(dna.DnaCodeMisalignedException):
-                dna.Dna(code=dna_code[-1:], gene_len=self.gene_len)
+    def test_read_genes_for_1_gene_len_2(self):
+        self.assertEqual([[0.1, 0.2]], dna.Dna.read("0.1, 0.2, 1", gene_len=2).genes)
 
-    def test_get_genes_expresses_all_genes_for_control_genes_all_above_treshold(self):
-        actual = self.subject.express()
+    def test_read_genes_for_1_gene_len_3(self):
+        self.assertEqual([[0.1, 0.2, 0.3]], dna.Dna.read("0.1, 0.2, 0.3, 1", gene_len=3).genes)
+
+    def test_read_genes_for_2_gene_len_2(self):
+        self.assertEqual([[0.1, 0.2], [0.3, 0.4]], dna.Dna.read("0.1, 0.2, 0.3, 0.4, 1, 1", gene_len=2).genes)
