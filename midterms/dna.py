95ddf2ee4f3a9a93f6d2fd9b13b225a38b55e7fd
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-08 23:11:31 +0100

    dna: better attribute names

--- midterms/dna.py
+++ midterms/dna.py
@@ -5,15 +5,15 @@ from typing import List, Tuple
 @dataclass
 class Dna:
     code: List[float]
-    genes: List[List[float]]
-    controls: List[bool]
+    genes_features: List[List[float]]
+    genes_control: List[bool]
 
     @staticmethod
     def read(data: str, gene_len: int = 1) -> "Dna":
         assert data and len(data.split(',')) > gene_len
         dna_code = Dna._read_dna_code_from(data)
-        genes, controls = Dna._read_genes_from(dna_code, gene_len)
-        return Dna(code=dna_code, genes=genes, controls=controls)
+        features, controls = Dna._read_genes_from(dna_code, gene_len)
+        return Dna(code=dna_code, genes_features=features, genes_control=controls)
 
     @staticmethod
     def _read_dna_code_from(data: str) -> List[float]:
@@ -21,13 +21,13 @@ class Dna:
 
     @staticmethod
     def _read_genes_from(dna_code: List[float], gene_len: int) -> Tuple[List[List[float]], List[bool]]:
-        genes: List[List[float]] = []
+        features: List[List[float]] = []
         controls: List[bool] = []
         i, end = 0, len(dna_code)
         while i < end:
-            genes.append(dna_code[i:i+gene_len])
+            features.append(dna_code[i:i+gene_len])
             i += gene_len
             end -= 1
         for i in range(end, len(dna_code)):
             controls.append(dna_code[i] > 0.5)
-        return genes, controls
+        return features, controls
--- midterms/test_dna.py
+++ midterms/test_dna.py
@@ -16,30 +16,30 @@ class DnaTest(unittest.TestCase):
 
     def test_read_for_1_gene_len_1(self):
         actual = dna.Dna.read("0.5, 1", gene_len=1)
-        self.assertEqual([[0.5]], actual.genes)
-        self.assertEqual([True], actual.controls)
+        self.assertEqual([[0.5]], actual.genes_features)
+        self.assertEqual([True], actual.genes_control)
 
     def test_read_for_2_gene_len_1(self):
         actual = dna.Dna.read("0.1, 0.2, 0, 1", gene_len=1)
-        self.assertEqual([[0.1], [0.2]], actual.genes)
-        self.assertEqual([False, True], actual.controls)
+        self.assertEqual([[0.1], [0.2]], actual.genes_features)
+        self.assertEqual([False, True], actual.genes_control)
 
     def test_read_for_3_gene_len_1(self):
         actual = dna.Dna.read("0.1, 0.2, 0.3, 0.5, 0.8, 0.2", gene_len=1)
-        self.assertEqual([[0.1], [0.2], [0.3]], actual.genes)
-        self.assertEqual([False, True, False], actual.controls)
+        self.assertEqual([[0.1], [0.2], [0.3]], actual.genes_features)
+        self.assertEqual([False, True, False], actual.genes_control)
 
     def test_read_for_1_gene_len_2(self):
         actual = dna.Dna.read("0.1, 0.2, 0.3", gene_len=2)
-        self.assertEqual([[0.1, 0.2]], actual.genes)
-        self.assertEqual([False], actual.controls)
+        self.assertEqual([[0.1, 0.2]], actual.genes_features)
+        self.assertEqual([False], actual.genes_control)
 
     def test_read_for_1_gene_len_3(self):
         actual = dna.Dna.read("0.1, 0.2, 0.3, 0.51", gene_len=3)
-        self.assertEqual([[0.1, 0.2, 0.3]], actual.genes)
-        self.assertEqual([True], actual.controls)
+        self.assertEqual([[0.1, 0.2, 0.3]], actual.genes_features)
+        self.assertEqual([True], actual.genes_control)
 
     def test_read_for_2_gene_len_2(self):
         actual = dna.Dna.read("0.1, 0.2, 0.3, 0.4, 0, 1", gene_len=2)
-        self.assertEqual([[0.1, 0.2], [0.3, 0.4]], actual.genes)
-        self.assertEqual([False, True], actual.controls)
+        self.assertEqual([[0.1, 0.2], [0.3, 0.4]], actual.genes_features)
+        self.assertEqual([False, True], actual.genes_control)
