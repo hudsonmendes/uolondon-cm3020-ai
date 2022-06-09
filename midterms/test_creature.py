7738c27cd3091431d7a9c708c4ef80135657d5f5
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-09 01:48:07 +0100

    creature/body: children test failing

--- midterms/test_creature.py
+++ midterms/test_creature.py
@@ -10,16 +10,28 @@ from creature import Creature
 class CreatureTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.dna = Dna.parse_dna(data=[1.] * Phenotype.gen_len(), gene_len=Phenotype.gen_len())
+        dna_code = (
+            [1.] * Phenotype.gen_len() + # trunk
+            [0.] * Phenotype.gen_len() + # limb-1
+            [1, 1]
+        )
+        self.dna = Dna.parse_dna(data=dna_code, gene_len=Phenotype.gen_len())
+        self.creature = Creature.develop_from(dna=self.dna)
 
     def test_class_exists(self):
         self.assertIsNotNone(Creature)
 
     def test_creature_dna(self):
-        actual = Creature.develop_from(dna=self.dna)
-        self.assertEqual(self.dna, actual.dna)
+        self.assertEqual(self.dna, self.creature.dna)
 
-    def test_creature_parts_root(self):
-        actual = Creature.develop_from(dna=self.dna)
-        self.assertIsNotNone(actual.body)
-        self.assertIsNone(actual.body.parent)
+    def test_creature_body_is_not_none(self):
+        self.assertIsNotNone(self.creature.body)
+
+    def test_creature_body_parent_is_none(self):
+        self.assertIsNone(self.creature.body.parent)
+
+    def test_creature_body_children_is_not_none(self):
+        self.assertIsNotNone(self.creature.body.children)
+
+    def test_creature_body_children_has_size_1(self):
+        self.assertEqual(1, len(self.creature.body.children))
