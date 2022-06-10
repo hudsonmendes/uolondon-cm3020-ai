83c8463946a01e3af934fe2334aafb440ce69670
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-10 19:24:17 +0100

    primordial soup: genes are all expressed

--- midterms/primordial_soup.py
+++ midterms/primordial_soup.py
@@ -11,7 +11,7 @@ class PrimordialSoup:
     @staticmethod
     def spark_life(gene_count: int) -> List[float]:
         genes_code = [PrimordialSoup.spark_gene() for _ in range(gene_count)]
-        genes_code += [[random.random() for _ in range(gene_count)]]
+        genes_code += [[1. for _ in range(gene_count)]]
         dna_code = list(itertools.chain(*genes_code))
         return dna_code
 
