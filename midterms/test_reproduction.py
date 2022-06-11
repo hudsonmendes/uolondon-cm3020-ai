46aa8d17c70ec40fc3398bfaf6065f6cb1668d82
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-11 17:13:05 +0100

    removed undeterministic tests

--- midterms/test_reproduction.py
+++ midterms/test_reproduction.py
@@ -28,30 +28,6 @@ class ReproductionTest(unittest.TestCase):
         child = self.reproduction.reproduce(a=self.adam, b=self.eve)
         self.assertGreater(len(child), 0)
 
-    def test_reproduce_applies_point_mutation_when_enabled(self):
-        reproduction_pme = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=True, shrink_mutation_enabled=False, grow_mutation_enabled=False))
-        reproduction_pmd = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=False, shrink_mutation_enabled=False, grow_mutation_enabled=False))
-        actual_pme = reproduction_pme.reproduce(a=self.adam, b=self.eve)
-        actual_pmd = reproduction_pmd.reproduce(a=self.adam, b=self.eve)
-        not_matching = [1 for (a, b) in zip(actual_pme, actual_pmd) if a != b]
-        self.assertGreater(len(not_matching), 0)
-
-    def test_reproduce_applies_shrink_mutation_when_enabled(self):
-        reproduction_sme = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=False, shrink_mutation_enabled=True, grow_mutation_enabled=False))
-        reproduction_smd = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=False, shrink_mutation_enabled=False, grow_mutation_enabled=False))
-        actual_sme = reproduction_sme.reproduce(a=self.adam, b=self.eve)
-        actual_smd = reproduction_smd.reproduce(a=self.adam, b=self.eve)
-        self.assertNotEqual(actual_sme, actual_smd)
-        self.assertLess(len(actual_sme), len(actual_smd))
-
-    def test_reproduce_applies_grow_mutation_when_enabled(self):
-        reproduction_gme = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=False, shrink_mutation_enabled=False, grow_mutation_enabled=True))
-        reproduction_gmd = Reproduction(settings=ReproductiveSettings(point_mutation_enabled=False, shrink_mutation_enabled=False, grow_mutation_enabled=False))
-        actual_gme = reproduction_gme.reproduce(a=self.adam, b=self.eve)
-        actual_gmd = reproduction_gmd.reproduce(a=self.adam, b=self.eve)
-        self.assertNotEqual(actual_gme, actual_gmd)
-        self.assertGreater(len(actual_gmd), len(actual_gme))
-
     def test_crossover_dna_contains_left_sequence_from_adam(self):
         child = self.reproduction._crossover(a=self.adam, b=self.eve).tolist()
         diff_from_adam = [i for i in range(len(child)) if i >= len(self.adam) or child[i] != self.adam[i]]
@@ -83,19 +59,19 @@ class ReproductionTest(unittest.TestCase):
 
     def test_shrink_mutation_never_removes_more_than_gen_len(self):
         reproduction = Reproduction(settings=ReproductiveSettings(shrink_mutation_rate=1.))
-        before = [random.random() for _ in range(Gene.length() * random.randint(1, 15))]
+        before = [random.random() for _ in range(Gene.length() * random.randint(5, 15))]
         after = reproduction._mutate_shrink(before)
         self.assertEqual(len(after), Gene.length())
     
     def test_shrink_mutation_removes_some_genetic_code_when_half(self):
         reproduction = Reproduction(settings=ReproductiveSettings(shrink_mutation_rate=0.5))
-        before = [random.random() for _ in range(Gene.length() * random.randint(1, 15))]
+        before = [random.random() for _ in range(Gene.length() * random.randint(5, 15))]
         after = reproduction._mutate_shrink(before)
         self.assertLess(len(after), int(len(before)))
 
     def test_shrink_mutation_does_not_remove_genetic_code_when_zero(self):
         reproduction = Reproduction(settings=ReproductiveSettings(shrink_mutation_rate=0.))
-        before = [random.random() for _ in range(Gene.length() * random.randint(1, 15))]
+        before = [random.random() for _ in range(Gene.length() * random.randint(5, 15))]
         after = reproduction._mutate_shrink(before)
         self.assertEqual(len(after), len(before))
 
