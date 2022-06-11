d4940a6bb148249a6a69a3f048d78c527735c491
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-11 11:25:14 +0100

    motor: pulse dealing with frequency

--- midterms/motor.py
+++ midterms/motor.py
@@ -1,13 +1,20 @@
-from dataclasses import dataclass
+from typing import Iterator
+
+from numpy import pi, sin as sine
 
 from phenotype import Phenotype, PhenotypeWaveForm
 
 
-@dataclass
-class Motor:
+class Motor(Iterator):
     waveform: PhenotypeWaveForm
     amp: float
     freq: float
+    phase: float = 0.
+
+    def __init__(self, waveform: PhenotypeWaveForm, amp: float, freq: float):
+        self.waveform = waveform
+        self.amp = amp
+        self.freq = freq
 
     @staticmethod
     def generate_from(phenotype: Phenotype) -> "Motor":
@@ -17,4 +24,14 @@ class Motor:
             freq=phenotype.control_freq
         )
 
-    
+    def __iter__(self) -> "Motor":
+        return self
+
+    def __next__(self) -> float:
+        output = 0.
+        self.phase = (self.phase + self.freq) % (2 * pi)
+        if self.waveform == PhenotypeWaveForm.PULSE:
+            output = 1. if self.phase < pi else -1.
+        elif self.waveform == PhenotypeWaveForm.SINE:
+            output = sine(self.phase)
+        return output
--- midterms/test_motor.py
+++ midterms/test_motor.py
@@ -1,8 +1,9 @@
-from typing import Union
-
-from unittest.mock import patch, Mock
 import unittest
-import random
+from unittest.mock import patch
+from hypothesis import given
+from hypothesis.strategies import sampled_from, floats, integers
+
+from numpy import pi
 
 from motor import Motor
 from phenotype import PhenotypeWaveForm
@@ -13,13 +14,29 @@ class MotorTest(unittest.TestCase):
     def test_class_exists(self):
         self.assertIsNotNone(Motor)
 
-    @patch("phenotype.Phenotype")
-    def test_parse_from_phenotype(self, MockPhenotype: Mock):
-        waveform, amp, freq = PhenotypeWaveForm.PULSE, random.random(), random.random()
-        MockPhenotype.return_value.control_waveform = waveform
-        MockPhenotype.return_value.control_amp = amp
-        MockPhenotype.return_value.control_freq = freq
-        actual = Motor.generate_from(MockPhenotype())
-        self.assertEqual(waveform, actual.waveform)
-        self.assertEqual(amp, actual.amp)
-        self.assertEqual(freq, actual.freq)
+    @given(sampled_from(PhenotypeWaveForm), floats(allow_nan=False), floats(allow_nan=False))
+    def test_parse_from_phenotype(self, waveform: PhenotypeWaveForm, amp: float, freq: float):
+        with patch("phenotype.Phenotype") as MockPhenotype:
+            MockPhenotype.return_value.control_waveform = waveform
+            MockPhenotype.return_value.control_amp = amp
+            MockPhenotype.return_value.control_freq = freq
+            actual = Motor.generate_from(MockPhenotype())
+            self.assertEqual(waveform, actual.waveform)
+            self.assertEqual(amp, actual.amp)
+            self.assertEqual(freq, actual.freq)
+
+    @given(integers(1, 100))
+    def test_next_pulse_amp_1_freq_pi(self, n):
+        motor = Motor(waveform=PhenotypeWaveForm.PULSE, amp=1., freq=pi)
+        for _ in range(n):
+            self.assertEqual(-1., next(motor))
+            self.assertEqual(+1., next(motor))
+
+    @given(integers(1, 100))
+    def test_next_pulse_amp_1_freq_half_pi(self, n):
+        motor = Motor(waveform=PhenotypeWaveForm.PULSE, amp=1., freq=pi/2)
+        for _ in range(n):
+            self.assertEqual(+1., next(motor))
+            self.assertEqual(-1., next(motor))
+            self.assertEqual(-1., next(motor))
+            self.assertEqual(+1., next(motor))
