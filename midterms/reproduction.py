2bb71fdb5e66ad39d8e6cedda765e69bfef0db6d
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-11 15:48:38 +0100

    reproduction: crossover tests fixed

--- midterms/reproduction.py
+++ midterms/reproduction.py
@@ -1,27 +1,52 @@
+from dataclasses import dataclass
 from typing import List
 
+import copy
 import random
 
 import numpy as np
 
 
+@dataclass
+class ReproductiveSettings:
+    crossover_min_len = 0.25
+    crossover_max_len = 0.75
+    point_mutation_enabled: bool = True
+    point_mutation_rate: float = 0.1
+    point_mutation_amount: float = 0.1
+    shrink_mutation_enabled: bool = True
+    shrink_mutation_rate: float = 0.25
+    grow_mutation_enabled: bool = True
+    grow_mutation_rate: float = 0.1
+
+
+@dataclass
 class Reproduction:
     dna_code_a: List[float]
     dna_code_b: List[float]
-
-    def __init__(
-            self,
-            dna_code_a: List[float],
-            dna_code_b: List[float]) -> None:
-        self.dna_code_a = dna_code_a
-        self.dna_code_b = dna_code_b
+    settings: ReproductiveSettings = ReproductiveSettings()
 
     def reproduce(self) -> List[float]:
         dna = self._crossover()
+        if self.settings.point_mutation_enabled:
+            dna = self._point_mutate(dna)
         return dna
 
     def _crossover(self) -> List[float]:
-        x1 = random.randint(1, len(self.dna_code_a)-1)
-        x2 = random.randint(1, len(self.dna_code_b)-1)
+        x1_min = int(max(1, len(self.dna_code_a) * self.settings.crossover_min_len))
+        x1_max = int(min(len(self.dna_code_a), len(self.dna_code_a) * self.settings.crossover_max_len))
+        x2_min = int(max(1, len(self.dna_code_b) * self.settings.crossover_min_len))
+        x2_max = int(min(len(self.dna_code_b), len(self.dna_code_b) * self.settings.crossover_max_len))
+        x1 = random.randint(x1_min, x1_max)
+        x2 = random.randint(x2_min, x2_max)
         g3 = np.concatenate([self.dna_code_a[:x1], self.dna_code_b[x2:]])
         return g3.tolist()
+
+    def _point_mutate(self, before: List[float]) -> List[float]:
+        after = copy.copy(before)
+        for i in range(len(after)):
+            if random.random() < self.settings.point_mutation_rate:
+                after[i] += self.settings.point_mutation_amount
+                after[i] = min(0.99999, after[i])
+                after[i] = max(0.00001, after[i])
+        return after
--- midterms/test_reproduction.py
+++ midterms/test_reproduction.py
@@ -5,7 +5,7 @@ from hypothesis.strategies import integers
 
 import random
 
-from reproduction import Reproduction
+from reproduction import Reproduction, ReproductiveSettings
 from gene import Gene
 
 
@@ -15,29 +15,53 @@ class ReproductionTest(unittest.TestCase):
         self.adam = [random.random() for _ in range(Gene.length() * random.randint(1, 15))]
         self.eve = [random.random() for _ in range(Gene.length() * random.randint(1, 15))]
         self.reproduction = Reproduction(dna_code_a=self.adam, dna_code_b=self.eve)
-        self.child = self.reproduction.reproduce()
 
     def test_class_exists(self):
         self.assertIsNotNone(Reproduction)
 
-    def test_breed_generates_new_dna(self):
-        self.assertNotEqual(self.child, self.adam)
-        self.assertNotEqual(self.child, self.eve)
+    def test_reproduce_generates_new_dna(self):
+        child = self.reproduction.reproduce()
+        self.assertNotEqual(child, self.adam)
+        self.assertNotEqual(child, self.eve)
 
-    def test_breed_generates_dna_not_empty(self):
-        self.assertGreater(len(self.child), 0)
+    def test_reproduce_generates_dna_not_empty(self):
+        child = self.reproduction.reproduce()
+        self.assertGreater(len(child), 0)
 
-    def test_breed_dna_contains_left_sequence_from_adam(self):
-        diff_from_adam = [i for i in range(len(self.child)) if i >= len(self.adam) or self.child[i] != self.adam[i]]
+    def test_crossover_dna_contains_left_sequence_from_adam(self):
+        child = self.reproduction._crossover()
+        diff_from_adam = [i for i in range(len(child)) if i >= len(self.adam) or child[i] != self.adam[i]]
         limit = min(diff_from_adam) - 1
         self.assertGreater(len(self.adam[:limit]), 0)
         self.assertLess(len(self.adam[:limit]), len(self.adam))
-        self.assertListEqual(self.adam[:limit], self.child[:limit])
+        self.assertListEqual(self.adam[:limit], child[:limit])
 
-    def test_breed_dna_contains_right_sequence_from_eve(self):
-        diff_len = len(self.child) - len(self.eve)
-        diff_from_eve = [i for i in range(len(self.child)-1, -1, -1) if (i - diff_len) < 0 or self.child[i] != self.eve[i - diff_len]]
+    def test_reproduce_applies_point_mutation_when_enabled(self):
+        reproduction_pme = Reproduction(dna_code_a=self.adam, dna_code_b=self.eve, settings=ReproductiveSettings(point_mutation_rate=1., point_mutation_amount=1.))
+        reproduction_pmd = Reproduction(dna_code_a=self.adam, dna_code_b=self.eve, settings=ReproductiveSettings(point_mutation_enabled=False, point_mutation_rate=1., point_mutation_amount=1.))
+        actual_pme = reproduction_pme.reproduce()
+        actual_pmd = reproduction_pmd.reproduce()
+        self.assertNotEqual(actual_pme, actual_pmd)
+        self.assertTrue(all([base == 0.99999 for base in actual_pme]))
+        self.assertTrue(not any([base == 0.99999 for base in actual_pmd]))
+
+    def test_crossover_dna_contains_right_sequence_from_eve(self):
+        child = self.reproduction._crossover()
+        diff_len = len(child) - len(self.eve)
+        diff_from_eve = [i for i in range(len(child)-1, -1, -1) if (i - diff_len) < 0 or child[i] != self.eve[i - diff_len]]
         limit = max(diff_from_eve) + 1
-        self.assertGreater(len(self.child[limit:]), 0)
-        self.assertLess(len(self.eve[:limit]), len(self.eve))
-        self.assertListEqual(self.eve[limit-diff_len:], self.child[limit:])
+        self.assertGreater(len(child[limit:]), 0)
+        self.assertLess(len(self.eve[limit-diff_len:]), len(self.eve))
+        self.assertListEqual(self.eve[limit-diff_len:],child[limit:])
+
+    def test_point_mutation_adds_1_to_all_bases_with_rate_1(self):
+        settings = ReproductiveSettings(point_mutation_rate=1., point_mutation_amount=1.)
+        reproduction = Reproduction(dna_code_a=self.adam, dna_code_b=self.eve, settings=settings)
+        actual = reproduction.reproduce()
+        self.assertTrue(all([base == 0.99999 for base in actual]))
+
+    def test_point_mutation_affects_no_gene_to_all_bases_with_rate_0(self):
+        settings = ReproductiveSettings(point_mutation_rate=0., point_mutation_amount=1.)
+        reproduction = Reproduction(dna_code_a=self.adam, dna_code_b=self.eve, settings=settings)
+        actual = reproduction.reproduce()
+        self.assertTrue(not any([base == 0.99999 for base in actual]))
