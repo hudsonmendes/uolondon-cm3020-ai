d0216ea747dbc0fa788b03a4ceb9ac9ca8f4d4f1
Hudson Mendes <hudsonmendes@gmail.com>
2022-06-19 14:39:56 +0100

    experimentation reset

--- midterms/evaluation/tests.sh
+++ midterms/evaluation/tests.sh
@@ -3,17 +3,8 @@
 # ============================================
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.0/pmr=0.1/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.0 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.0/pmr=0.1/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.0 \
     --point_mutation_rate 0.1 \
@@ -22,7 +13,7 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.0/pmr=0.1/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.0 \
     --point_mutation_rate 0.1 \
@@ -30,26 +21,8 @@ python cli.py evolution optimise \
 
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.0/pmr=0.1/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.0 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount 0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.0/pmr=0.25/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.0 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.0/pmr=0.25/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.0 \
     --point_mutation_rate 0.25 \
@@ -58,37 +31,19 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.0/pmr=0.25/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.0 \
     --point_mutation_rate 0.25 \
     --point_mutation_amount 0.05
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.0/pmr=0.25/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.0 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount 0.15
 
 # ============================================
 # Atavism at 10.0%
 # ============================================
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.1/pmr=0.1/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.1 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.1/pmr=0.1/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.1 \
     --point_mutation_rate 0.1 \
@@ -97,7 +52,7 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.1/pmr=0.1/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.1 \
     --point_mutation_rate 0.1 \
@@ -105,26 +60,8 @@ python cli.py evolution optimise \
 
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.1/pmr=0.1/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.1 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount 0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.1/pmr=0.25/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.1 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.1/pmr=0.25/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.1 \
     --point_mutation_rate 0.25 \
@@ -133,37 +70,19 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.1/pmr=0.25/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.1 \
     --point_mutation_rate 0.25 \
     --point_mutation_amount 0.05
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.1/pmr=0.25/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.1 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount 0.15
 
 # ============================================
 # Atavism at 25.0%
 # ============================================
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.25/pmr=0.1/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.25 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.25/pmr=0.1/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.25 \
     --point_mutation_rate 0.1 \
@@ -172,7 +91,7 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.25/pmr=0.1/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.25 \
     --point_mutation_rate 0.1 \
@@ -180,26 +99,8 @@ python cli.py evolution optimise \
 
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.25/pmr=0.1/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.25 \
-    --point_mutation_rate 0.1 \
-    --point_mutation_amount 0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.25/pmr=0.25/pma=-0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.25 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount -0.15
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.25/pmr=0.25/pma=-0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.25 \
     --point_mutation_rate 0.25 \
@@ -208,18 +109,9 @@ python cli.py evolution optimise \
 python cli.py evolution optimise \
     --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
     --target_folder "evolution/atavism=0.25/pmr=0.25/pma=0.05" \
-    --n_generations 100 \
+    --n_generations 75 \
     --population_size 100 \
     --expression_threshold 0.25 \
     --point_mutation_rate 0.25 \
     --point_mutation_amount 0.05
-
-python cli.py evolution optimise \
-    --genesis_filepath "/Users/hudsonmendes/Workspaces/hudsonmendes-estudos/cm3020-ai/midterms/evolution/genesis.gen" \
-    --target_folder "evolution/atavism=0.25/pmr=0.25/pma=0.15" \
-    --n_generations 100 \
-    --population_size 100 \
-    --expression_threshold 0.25 \
-    --point_mutation_rate 0.25 \
-    --point_mutation_amount 0.15
 
